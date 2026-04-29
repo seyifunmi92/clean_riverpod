9750e4cbc1177a69996681cd30a34bbf885d61c6
Oluwaseyi Fatunmole <ofatunmole@itandtsolutions.net>
2026-04-29 07:01:23 +0100

    added riverpod

---
+++ lib/core/app/di/providers.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:clean_architechture_kit_riverpod/core/network/api/base.dart';
+import 'package:clean_architechture_kit_riverpod/core/network/api/base_impl.dart';
+import 'package:clean_architechture_kit_riverpod/core/network/settings/entry.dart';
+import 'package:clean_architechture_kit_riverpod/core/base/data_source/base_data_source.dart';
+
+final network = Provider<NetworkSelector>((ref) => NetworkSelector.dio);
+final api = Provider<ApiBase>((ref) => Api(base: ref.watch(network).api));
+final baseDataSource = Provider<BaseDataSource>((ref) => BaseDataSource(ref.watch(api)));
